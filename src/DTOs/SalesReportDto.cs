7a956a2a080fcbddb0b099bbf0f78a9e232cee3c
LuisMoura1 <163213780+LuisMoura1@users.noreply.github.com>
2026-05-10 22:56:27 -0300

    Add files via upload

---
+++ src/DTOs/SalesReportDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EcommerseEscalavel.DTOs
+{
+    public class SalesReportDto
+    {
+        public int TotalOrders { get; set; }
+        public int TotalItemsSold { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public TopProductDto TopProduct { get; set; }
+    }
+}
---
+++ src/DTOs/TopProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerseEscalavel.DTOs
+{
+    public class TopProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int TotalSold { get; set; }
+    }
+}
